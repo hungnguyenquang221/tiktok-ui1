a2a42ca3e9855c63d25a1588c434ebc87c48131a
NguyenHung <hungnguyenquang221@gmail.com>
2023-09-06 18:08:34 +0700

    c

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,11 +1,22 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles);
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="TikTok" />
+                </div>
+            </div>
+            <div className={cx('search')}>
+                <input placeholder="Search account and videos" spellCheck={false} />
+                <button className={cx('clear')}></button>
+
+                <button className={cx('search-button')}></button>
+            </div>
         </header>
     );
 }
