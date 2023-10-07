25a5c7e4c0e2e41fe46eb7c3361b825ab6fcfb93
hungnguyen <hungnguyenquang221@gmail.com>
2023-10-07 22:14:41 +0700

    change all

---
+++ src/components/Image/Image.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import { useState, forwardRef } from 'react';
+import images from '~/assets/images';
+import styles from './Image.module.scss';
+import classNames from 'classnames';
+const Image = forwardRef(({ className, src, alt, fallback: customFallback = images.noImage, ...props }, ref) => {
+    const [fallback, setFallback] = useState('');
+    const handleError = () => {
+        setFallback(customFallback);
+    };
+    // eslint-disable-next-line jsx-a11y/alt-text
+    return (
+        <img
+            className={classNames(styles.wrapper, className)}
+            ref={ref}
+            src={fallback || src}
+            alt={alt}
+            {...props}
+            onError={handleError}
+        />
+    );
+});
+Image.propTypes = {
+    className: PropTypes.string,
+    src: PropTypes.string,
+    alt: PropTypes.string,
+    fallback: PropTypes.string,
+};
+export default Image;
