72d077100c14d62aba4359e64e608540b01b1b9f
hungnguyen <hungnguyenquang221@gmail.com>
2023-10-09 23:49:44 +0700

    change

---
+++ src/layouts/components/Sidebar/Menu/Menu.js
@@ -0,0 +1,6 @@
+import PropTypes from 'prop-types';
+function Menu({ children }) {
+    return <nav>{children}</nav>;
+}
+Menu.proTypes = { children: PropTypes.node.isRequired };
+export default Menu;
---
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -0,0 +1,22 @@
+import classNames from 'classnames/bind';
+import styles from './Sidebar.module.scss';
+import Menu, { MenuItem } from './Menu';
+import { HomeIcon, UserGroupIcon, LiveIcon } from '~/components/Icons';
+import config from '~/config';
+
+const cx = classNames.bind(styles);
+
+function Sidebar() {
+    return (
+        <aside className={cx('wrapper')}>
+            <Menu>
+                <MenuItem title="For your" to={config.routes.home} icon={<HomeIcon />} />
+
+                <MenuItem title="Following" to={config.routes.following} icon={<UserGroupIcon />} />
+                <MenuItem title="Live" to={config.routes.live} icon={<LiveIcon />} />
+            </Menu>
+        </aside>
+    );
+}
+
+export default Sidebar;
