6dfe251666a20f3393af3262fcd138f3b493ff21
hungnguyen <hungnguyenquang221@gmail.com>
2023-10-10 22:43:56 +0700

    change

--- src/layouts/components/Sidebar/Menu/MenuItem.js
+++ src/layouts/components/Sidebar/Menu/MenuItem.js
@@ -4,10 +4,11 @@ import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
-function MenuItem({ title, to, icon }) {
+function MenuItem({ title, to, icon, activeIcon }) {
     return (
         <NavLink className={(nav) => cx('menu-item', { active: nav.isActive })} to={to}>
-            {icon}
+            <span className={cx('icon')}>{icon}</span>
+            <span className={cx('active-icon')}>{activeIcon}</span>
             <span className={cx('title')}>{title}</span>
         </NavLink>
     );
